8f286765e0270aa1c9be80f7b5c7d98a59ef5102
pro5502 <79224315+pro5502@users.noreply.github.com>
2021-02-17 19:41:13 +0000

    Add files via upload

---
+++ main.js
@@ -0,0 +1,88 @@
+canvas=document.getElementById('myCanvas');
+ctx=canvas.getContext("2d");
+
+var rover_width=100;
+var rover_height=90;
+
+rover_x=10;
+rover_y=10;
+
+background_img="mars.jpg";
+rover_img="rover.png";
+
+function add() {
+    background_imgTag=new Image();
+    background_imgTag.onload=uploadBackground;
+    background_imgTag.src=background_img;
+
+    rover_imgTag=new Image();
+    rover_imgTag.onload=uploadrover;
+    rover_imgTag.src=rover_img;   
+}
+
+function uploadBackground() {
+ctx.drawImage(background_imgTag,0,0, canvas.width, canvas.height);
+}
+function uploadrover() {
+    ctx.drawImage(rover_imgTag,rover_x,rover_y, rover_width, rover_height);
+    }
+
+    window.addEventListener("keydown",my_keydown);
+
+    function my_keydown(e) {
+        keyPressed=e.keyCode;
+        
+        if (keyPressed=='38') {
+            up();
+        }
+
+        if (keyPressed=='40') {
+            down();
+        }
+
+        if (keyPressed=='37') {
+            left();
+        }
+
+        if (keyPressed=='39') {
+            right();
+        }
+
+    
+    }
+
+    function up() {
+        if (rover_y>=0) {
+            rover_y=rover_y-10;
+
+         uploadBackground();
+         uploadrover();
+        }
+    }
+
+    function down() {
+        if (rover_y<=500) {
+            rover_y=rover_y+10;
+
+         uploadBackground();
+         uploadrover();
+        }
+    }
+
+    function left() {
+        if (rover_x>=0) {
+            rover_x=rover_x-10;
+
+         uploadBackground();
+         uploadrover();
+        }
+    }
+
+    function right() {
+        if (rover_x<=700) {
+            rover_x=rover_x+10;
+
+         uploadBackground();
+         uploadrover();
+        }
+    }
